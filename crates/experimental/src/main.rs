74f8c65436711ed2f019fc3999746981d75e4b9e
jonas <jonaspfeifer@drei.at>
2025-02-12 08:27:20 +0100

    changed to workspace

--- crates/experimental/src/main.rs
+++ crates/experimental/src/main.rs
@@ -1,5 +1,3 @@
-#![feature(map_try_insert)]
-
 use crate::thinking_layer::ThinkingLayer;
 use rand::{Rng, rng};
 use tokio::time::Instant;
@@ -10,14 +8,13 @@ mod thinking_layer;
 
 #[tokio::main]
 async fn main() {
-    let mut thinking_layer = ThinkingLayer::new(4, 1, 32);
-
+    let mut thinking_layer = ThinkingLayer::new(4, 1, 256);
     loop {
         let start = Instant::now();
         let input = vec![rng().random::<f32>(); 4];
         let output = thinking_layer.tick(Some(input.clone()));
         println!("{:7.5?} -> {:.5?} ({:.2?})", input, output, start.elapsed());
 
-        // tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+        tokio::time::sleep(std::time::Duration::from_millis(100)).await;
     }
 }
