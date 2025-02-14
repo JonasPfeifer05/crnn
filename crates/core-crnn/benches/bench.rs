f18ef44a438cd9f57db2639453dfd928cdc23375
tobinio <Tobias.frischmann1@gmail.com>
2025-02-14 09:06:39 +0100

    suggested changes

--- crates/core-crnn/benches/bench.rs
+++ crates/core-crnn/benches/bench.rs
@@ -1,22 +1,22 @@
 use core_crnn::activation_function::ActivationFunction;
 use core_crnn::thinking_layer::ThinkingLayer;
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    let mut small_model = ThinkingLayer::new(5, 25, 5, ActivationFunction::Other(|x| x)).unwrap();
-    let mut medium_model = ThinkingLayer::new(5, 250, 5, ActivationFunction::Other(|x| x)).unwrap();
-    let mut large_model = ThinkingLayer::new(5, 2500, 5, ActivationFunction::Other(|x| x)).unwrap();
+    let mut small_model = ThinkingLayer::new(4, 32, 8, ActivationFunction::Other(|x| x)).unwrap();
+    let mut medium_model = ThinkingLayer::new(4, 256, 8, ActivationFunction::Other(|x| x)).unwrap();
+    let mut large_model = ThinkingLayer::new(4, 2048, 8, ActivationFunction::Other(|x| x)).unwrap();
 
     c.bench_function("small", |b| {
-        b.iter(|| small_model.tick(black_box(Some(vec![0., 0., 0., 0., 0.]))))
+        b.iter(|| small_model.tick(Some(vec![0., 0., 0., 0.])))
     });
 
     c.bench_function("medium", |b| {
-        b.iter(|| medium_model.tick(black_box(Some(vec![0., 0., 0., 0., 0.]))))
+        b.iter(|| medium_model.tick(Some(vec![0., 0., 0., 0.])))
     });
 
     c.bench_function("large", |b| {
-        b.iter(|| large_model.tick(black_box(Some(vec![0., 0., 0., 0., 0.]))))
+        b.iter(|| large_model.tick(Some(vec![0., 0., 0., 0.])))
     });
 }
 
